0a42d24312449b97b8c5ab701f22d8d6fe5d8525
abdi <76499323+abdi10bands@users.noreply.github.com>
2020-12-28 01:37:05 -0800

    Update client1.cpp

--- client1.cpp
+++ client1.cpp
@@ -1,10 +1,7 @@
-//CS311 Yoshii
-//INSTRUCTION:
-//Look for ** to complete this program (Use control-S)
-//The output should match my hw1stackDemo.out 
+
 
 //=========================================================
-//HW#: HW1P1 stack application (post-fix evaluation)
+// stack application (post-fix evaluation)
 //Your name: Abdiel Verdin
 //Complier:  g++
 //File type: client program client1.cpp
